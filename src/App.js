f9351f97b9758f3d51135ef33d7e3d14fa31a075
sajid365-sr <sajidsorker86@gmail.com>
2022-11-14 02:56:17 +0600

    all important package installed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div>
-      
-
-
+      <h1>Client side is running</h1>
+      <button className="btn btn-primary">Click Me</button>
+      <button className="btn btn-outline">Click Me</button>
     </div>
   );
 }

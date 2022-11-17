9343dfe8b845ffc9c63d160eed4ac101d253be15
sajid365-sr <sajidsorker86@gmail.com>
2022-11-18 00:19:24 +0600

    all service card done

--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -5,11 +5,11 @@ import ServiceCard from './ServiceCard';
 
 const Services = () => {
     const services = useLoaderData();
-    console.log(services)
+   
     return (
         <div>
             <h1 className='text-4xl font-semibold text-[#0E3D4B] text-center my-10'>Explore My Services</h1>
-            <div className='grid grid-cols-3 container mx-auto mb-10'>
+            <div className='grid grid-cols-1 w-10/12 lg:w-full lg:grid-cols-3 container mx-auto mb-10'>
             {
                 services.map(service => <ServiceCard
                 key={service.id}
--- src/Routes/PublicRoutes/PublicRoutes.js
+++ src/Routes/PublicRoutes/PublicRoutes.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main/Main";
 import Blog from "../../Pages/Blog/Blog";
 import Home from "../../Pages/Home/Home";
+import ServiceDetails from "../../Pages/Services/ServiceDetails";
 import Services from "../../Pages/Services/Services";
 import Login from "../../Pages/Shared/Register/Login/Login";
 import SignUp from "../../Pages/Shared/Register/SignUp/SignUp";
@@ -25,6 +26,10 @@ export const router = createBrowserRouter([
                 path:'/services',
                 loader: () => fetch('http://localhost:5000/services'),
                 element: <Services></Services>
+            },
+            {
+                path:'/services/:id',
+                element:<ServiceDetails></ServiceDetails>
             },
             {
                 path:'/blog',
